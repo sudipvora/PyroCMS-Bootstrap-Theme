51e6655e424126c043bc9efa1a345ecfc985f339
RCrowt <richard@siylc.com>
2017-03-18 18:29:45 +0000

    Theme initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitter Bootstrap Theme for PyroCMS 2.2
+This theme is built for [PyroCMS 2.2](https://github.com/pyrocms/pyrocms/tree/2.2/master) which adds CSM functionality to the Codeignitor framework.
---
+++ theme.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Bootstrap extends Theme {
+
+    public $name = 'PyroCMS Test Theme';
+    public $author = 'Richard Crowther';
+    public $author_website = 'http://www.richardcrowther.com/';
+    public $website = 'http://www.richardcrowther.com/';
+    public $description = 'Simple Bootstrap theme created for PyroCMS on Codeigniter';
+    public $version = '0.1.0';
+}
